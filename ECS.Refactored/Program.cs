7dc73de91bf0204eb0e4887f6db8c612dab3ef44
tobiassandoelund <36983846+tobiassandoelund@users.noreply.github.com>
2019-02-12 14:03:54 +0100

    Upload af Refactored kode med Intefaces

---
+++ ECS.Refactored/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Refactored
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ecs = new ECS(28, new TempSensor(), new Heater());
+
+            ecs.Regulate();
+
+            ecs.SetThreshold(20);
+
+            ecs.Regulate();
+        }
+    }
+}
---
+++ ECS.Refactored/TempSensor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ECS.Refactored
+{
+    public class TempSensor : ITempSensor
+    {
+        private Random _gen = new Random();
+
+        public int GetTemp()
+        {
+            return _gen.Next(-5, 45);
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
