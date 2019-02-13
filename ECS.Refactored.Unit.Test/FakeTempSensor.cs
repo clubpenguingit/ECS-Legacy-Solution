1e4a1b2963c696de81e643ab68d1b47820ead4ca
clubpenguingit <clubpenguingit@gmail.com>
2019-02-13 11:30:04 +0100

    FakeTempSensor oprettet

---
+++ ECS.Refactored.Unit.Test/FakeTempSensor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Refactored.Unit.Test
+{
+    public class FakeTempSensor : ITempSensor
+    {
+
+        public int Temp { get; set; }
+
+        public int GetTemp()
+        {
+            return Temp;
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
