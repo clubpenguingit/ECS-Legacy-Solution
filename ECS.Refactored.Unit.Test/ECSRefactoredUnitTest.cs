1cd54f72666b21e1000a02c8f4a29850918ec5b4
tobiassandoelund <36983846+tobiassandoelund@users.noreply.github.com>
2019-02-13 12:22:32 +0100

    Tests

--- ECS.Refactored.Unit.Test/ECSRefactoredUnitTest.cs
+++ ECS.Refactored.Unit.Test/ECSRefactoredUnitTest.cs
@@ -19,7 +19,7 @@ namespace ECS.Refactored.Unit.Test
         {
             mockHeater = new FakeHeater();
             stubTempSensor = new FakeTempSensor();
-            uut = new ECS(75, stubTempSensor, mockHeater);
+            uut = new ECS(50, stubTempSensor, mockHeater);
         }
 
         [Test]
@@ -31,11 +31,71 @@ namespace ECS.Refactored.Unit.Test
         [Test]
         public void Regulate_TempExceedsThreshold_TurnOffHeater()
         {
-
             uut.SetThreshold(50);
             stubTempSensor.Temp = 55; // Above threshold
             uut.Regulate();       // Regulate will turn off heater. 
             Assert.That(mockHeater.HeaterState, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void Regulate_TempIsBelowThreshold_TurnOnHeater()
+        {
+            uut.SetThreshold(50);
+            stubTempSensor.Temp = 45; // Under threshold
+            uut.Regulate();       // Regulate will turn on heater. 
+            Assert.That(mockHeater.HeaterState, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Regulate_TempIsEqualToThreshold_TurnOffHeater()
+        {
+            uut.SetThreshold(50);
+            stubTempSensor.Temp = 50; // Under threshold
+            uut.Regulate();       // Regulate will turn on heater. 
+            Assert.That(mockHeater.HeaterState, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void SetThreshold_PlusDegree_HoldsGivenValue()
+        {
+            int thr = 50;
+            uut.SetThreshold(thr);
+            Assert.That(uut.GetThreshold(), Is.EqualTo(thr));
+        }
+
+        [Test]
+        public void SetThreshold_MinusDegree_HoldsGivenValue()
+        {
+            int thr = -150;
+            uut.SetThreshold(thr);
+            Assert.That(uut.GetThreshold(), Is.EqualTo(thr));
+        }
+
+        [Test]
+        public void SetThreshold_InsertZero_HoldsGivenValue()
+        {
+            int thr = 0;
+            uut.SetThreshold(thr);
+            Assert.That(uut.GetThreshold(), Is.EqualTo(thr));
+        }
+
+        /// <summary>
+        /// Lol
+        /// </summary>
+        [Test]
+        public void SetThreshold_BigVal_HoldsGivenValue()
+        {
+            int thr = 999999999;
+            uut.SetThreshold(thr);
+            Assert.That(uut.GetThreshold(), Is.EqualTo(thr));
+        }
+
+        [Test]
+        public void GetCurTemp_GetsTempFromStub_IsCorrect()
+        {
+            int temp = 50;
+            stubTempSensor.Temp = temp;
+            Assert.That(uut.GetCurTemp(), Is.EqualTo(temp));
         }
     }
 
