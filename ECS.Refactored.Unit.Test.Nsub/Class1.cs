0f2f45f296809277457001c19ddfc2af3989079a
tobiassandoelund <36983846+tobiassandoelund@users.noreply.github.com>
2019-02-19 13:18:48 +0100

    Tilføjet 2 tests

--- ECS.Refactored.Unit.Test.Nsub/Class1.cs
+++ ECS.Refactored.Unit.Test.Nsub/Class1.cs
@@ -11,6 +11,29 @@ namespace ECS.Refactored.Unit.Test.Nsub
     [TestFixture]
     public class EcsUnitTestNsubstitute
     {
-        
+        [Test]
+        public void Regulate_TempUnderThreshold_heaterIsOn()
+        {
+            var mockHeater = Substitute.For<IHeater>();
+            var stubSensor = Substitute.For<ITempSensor>();
+            stubSensor.GetTemp().Returns(45);
+            int threshold = 50;
+            ECS ecs = new ECS(threshold,stubSensor, mockHeater);
+            ecs.Regulate();
+            mockHeater.Received(1).TurnOn();
+        }
+
+        [Test]
+        public void Regulate_TempOverThreshold_heaterReceivedOff()
+        {
+            var mockHeater = Substitute.For<IHeater>();
+            var stubSensor = Substitute.For<ITempSensor>();
+            int threshold = 50;
+            stubSensor.GetTemp().Returns(51);
+            var uut = new ECS(threshold, stubSensor, mockHeater);
+            uut.Regulate();
+            mockHeater.Received(0).TurnOn();
+            mockHeater.Received(1).TurnOff();
+        }
     }
 }
