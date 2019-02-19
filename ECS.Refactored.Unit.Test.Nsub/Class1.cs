c80215eb95b6706157696434c2aa7d13cdd71845
tobiassandoelund <36983846+tobiassandoelund@users.noreply.github.com>
2019-02-19 12:57:33 +0100

    tilføjet Nsub testprojekt

---
+++ ECS.Refactored.Unit.Test.Nsub/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ECS.Refactored.Unit.Test.Nsub
+{
+    [TestFixture]
+    public class EcsUnitTestNsubstitute
+    {
+        
+    }
+}
